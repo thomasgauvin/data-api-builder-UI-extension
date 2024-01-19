c0dd5bf9e7b242faeddec876865e5a4a5b983c58
Aniruddh Munde <anmunde@microsoft.com>
2024-01-18 22:57:47 -0800

    Move ConfigurationPostParams to Config ObjectModel

--- src/Service/Controllers/ConfigurationController.cs
+++ src/Service/Controllers/ConfigurationController.cs
@@ -135,32 +135,4 @@ namespace Azure.DataApiBuilder.Service.Controllers
             return BadRequest();
         }
     }
-    /// <summary>
-    /// The required parameters required to configure the runtime.
-    /// </summary>
-    /// <param name="Configuration">The runtime configuration.</param>
-    /// <param name="Schema">The GraphQL schema. Can be left empty for SQL databases.</param>
-    /// <param name="ConnectionString">The database connection string.</param>
-    /// <param name="AccessToken">The managed identity access token (if any) used to connect to the database.</param>
-    /// <param name="Database"> The name of the database to be used for Cosmos</param>
-    public record class ConfigurationPostParameters(
-        string Configuration,
-        string? Schema,
-        string ConnectionString,
-        string? AccessToken)
-    { }
-
-    /// <summary>
-    /// The required parameters required to configure the runtime.
-    /// </summary>
-    /// <param name="Configuration">The runtime configuration.</param>
-    /// <param name="ConfigurationOverrides">Configuration parameters that override the options from the Configuration file.</param>
-    /// <param name="Schema">The GraphQL schema. Can be left empty for SQL databases.</param>
-    /// <param name="AccessToken">The managed identity access token (if any) used to connect to the database.</param>
-    public record class ConfigurationPostParametersV2(
-        string Configuration,
-        string ConfigurationOverrides,
-        string? Schema,
-        string? AccessToken)
-    { }
 }
